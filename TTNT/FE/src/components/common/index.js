54b3d31ef3a9de55c54773e4b21bdf7fb830acd7
lduc292004 <lduc292004@gmail.com>
2026-01-19 16:00:29 +0700

    Delete TTNT directory

--- TTNT/FE/src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Loading } from "./Loading";
-export { default as EmptyState } from "./EmptyState";
-export { default as Button } from "./Button";
-export { default as Card } from "./Card";
-export { default as Pagination } from "./Pagination";
-
